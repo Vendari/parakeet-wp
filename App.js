9f3bf40df429f7cbe30f65d40e044203fbfa3587
Jakub Brodecki <jakub.brodecki@vendari.dev>
2021-02-16 20:05:18 +0100

    remove tutorial part

--- App.js
+++ App.js
@@ -3,9 +3,6 @@ import { StyleSheet, View } from 'react-native';
 import { Provider } from 'react-redux';
 import store from './store';
 
-// pages
-import HomePage from './src/pages/Homepage/Homepage';
-
 // layout components
 import Notification from './src/components/Notification';
 
@@ -13,7 +10,6 @@ export default function App() {
   return (
     <Provider store={store}>
       <View style={styles.container}>
-        <HomePage/>
       </View>
       <Notification />
     </Provider>
